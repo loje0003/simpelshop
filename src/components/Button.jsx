11aba7e81cdf036dd5c8fa6579579f4ad87d014a
josefine-krarup <joskr0005@stud.kea.dk>
2026-04-20 17:01:31 +0200

    details done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,5 @@
 const Button = ({ children }) => {
-  return <button className="px-5 py-2 rounded-full border border-blue-950 bg-yellow-200 text-blue-950 text-xl font-medium">{children}</button>;
+  return <button className="px-5 py-2 rounded-full border border-blue-950 bg-yellow-200 text-blue-950 text-xl font-medium cursor-pointer">{children}</button>;
 };
 
 export default Button;
