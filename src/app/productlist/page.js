d09e321536c49cc4a6c5335506127d9473220d98
josefine-krarup <joskr0005@stud.kea.dk>
2026-04-20 17:44:33 +0200

    work done

--- src/app/productlist/page.js
+++ src/app/productlist/page.js
@@ -7,7 +7,7 @@ const List = ({ searchParams }) => {
   return (
     <div>
       <h1 className="text-7xl text-blue-950 mx-10 mt-5">All Products</h1>
-      <div className="flex justify-between mr-40">
+      <div className="flex justify-between mr-30 mt-5">
         <Category />
         <Search />
       </div>
--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,10 +1,30 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
 const Category = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const handleFilter = (category) => {
+    const params = new URLSearchParams(searchParams);
+
+    // toggle filter
+    if (params.get("category") === category) {
+      params.delete("category");
+    } else {
+      params.set("category", category);
+    }
+
+    router.push("?" + params.toString());
+  };
+
   return (
-    <div className="flex gap-4 p-4 mx-7 text-center">
-      <p className="px-5 py-4 rounded-full bg-yellow-200 text-blue-950 border border-blue-950 font-medium">Confident</p>
-      <p className="px-5 py-4 rounded-full bg-yellow-200 text-blue-950 border border-blue-950 font-medium">Alert</p>
-      <p className="px-5 py-4 rounded-full bg-yellow-200 text-blue-950 border border-blue-950 font-medium">Playful</p>
-      <p className="px-5 py-4 rounded-full bg-yellow-200 text-blue-950 border border-blue-950 font-medium">Loyal</p>
+    <div className="flex gap-4 p-4 mx-7">
+      <button onClick={() => handleFilter("Beauty")}>Beauty</button>
+      <button onClick={() => handleFilter("Groceries")}>Groceries</button>
+      <button onClick={() => handleFilter("Fragrances")}>Fragrances</button>
+      <button onClick={() => handleFilter("Furniture")}>Furniture</button>
     </div>
   );
 };
