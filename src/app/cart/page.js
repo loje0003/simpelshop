25d98a8c48e282ec9af6294252f54c5a656ed5bf
loje0003 <loje0003@stud.kea.dk>
2026-04-22 11:28:44 +0200

    rettelser

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -2,7 +2,10 @@ import Kurv from "@/components/Kurv";
 
 const CartPage = () => {
   return (
-
+    <div className="p-5">
+      <h1 className="text-7xl text-blue-950 font-bold mb-4">Indkøbskurv</h1>
+      <Kurv />
+    </div>
   );
 };
 
