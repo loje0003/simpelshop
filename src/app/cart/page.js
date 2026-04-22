3670321347ff6c0102587f34cf55f4a0d5d3ed16
loje0003 <loje0003@stud.kea.dk>
2026-04-22 11:26:16 +0200

    start-kruven

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -1,26 +1,10 @@
-"use client";
-
-import { useCartStore } from "@/store/cartStore";
+import Kurv from "@/components/Kurv";
 
 const CartPage = () => {
-  const cart = useCartStore((state) => state.cart);
-  const removeFromCart = useCartStore((state) => state.removeFromCart);
-
   return (
-    <div>
-      <h1>Cart</h1>
-
-      {cart.length === 0 && <p>Din kurv er tom</p>}
-
-      {cart.map((item) => (
-        <div key={item.id}>
-          <h2>{item.title}</h2>
-          <p>{item.price} kr</p>
-          <p>Antal: {item.quantity}</p>
-
-          <button onClick={() => removeFromCart(item.id)}>Fjern</button>
-        </div>
-      ))}
+    <div className="p-5">
+      <h1 className="text-7xl text-blue-950 font-bold mb-4">Indkøbskurv</h1>
+      <Kurv />
     </div>
   );
 };
