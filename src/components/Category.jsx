f6dcbaf1dbae033c1cdce3ec36be1e366dddfb92
josefine-krarup <joskr0005@stud.kea.dk>
2026-04-22 11:32:29 +0200

    fix donedone

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -20,7 +20,7 @@ const Category = () => {
   };
 
   return (
-    <div className="flex items-center gap-4 ">
+    <div className="flex items-center gap-4 ml-4 ">
       <button className="px-5 py-2 rounded-full border border-blue-950 bg-yellow-200 text-blue-950 text-xl font-medium cursor-pointer" onClick={() => handleFilter("Beauty")}>
         Beauty
       </button>
